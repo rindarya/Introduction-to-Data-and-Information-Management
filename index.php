8a52e35570d4e68bbdb81c713c38f6fb97436baf
Rinda Aryani <rindaaryani12@gmail.com>
2023-10-22 17:13:33 +0700

    (FEATURE) create index.php as main entry point

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+
+?>
+
+<HTML>
+
+<HEAD>
+	<TITLE> Introduction to Data and Information Management </TITLE>
+</HEAD>
+
+<BODY>
+	<H1>Introduction to Data and Information Management</H1>
+</BODY>
+
+</HTML>
